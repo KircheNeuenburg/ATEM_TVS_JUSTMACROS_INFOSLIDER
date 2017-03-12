2ab5372470f58d5d2becd0bfc78616fbc86dcb0d
David Lang <dlang9159@users.noreply.github.com>
2017-03-12 10:01:03 +0100

    Fixed bug that overwrote images
    Index of generated jpg images started with 0, because of this previously generated png images were overwritten

--- scripts/1_convert_images_to_tvs_resolution.bat
+++ scripts/1_convert_images_to_tvs_resolution.bat
@@ -4,5 +4,5 @@ REM empty the output folder to prevent conflicts
 FOR %%f IN (InfoSlides\*) DO DEL "%%f"
 
 REM # convert images to 1920x1080 resolution, keeps aspect ratio and fills empty space with black
-magick convert "InputImages\*.png"   -resize 1920x1080 -background black -gravity center -extent 1920x1080 "InfoSlides\Slide.png"
-magick convert "InputImages\*.jpg"   -resize 1920x1080 -background black -gravity center -extent 1920x1080 "InfoSlides\Slide.png"
+magick convert "InputImages\*.png"   -resize 1920x1080 -background black -gravity center -extent 1920x1080 "I nfoSlides\SlidePNG.png"
+magick convert "InputImages\*.jpg"   -resize 1920x1080 -background black -gravity center -extent 1920x1080 "InfoSlides\SlideJPG.png"
