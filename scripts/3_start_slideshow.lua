f3077403f37904aeba915dce7bd7faf95b60044a
David Lang <dlang9159@users.noreply.github.com>
2017-03-29 08:30:01 +0200

    Initial commit of code

--- scripts/3_start_slideshow.lua
+++ scripts/3_start_slideshow.lua
@@ -1,1 +1,24 @@
--- fill lua code in here
+function InfoGraphic()
+    local media_player = 1;
+    while(true) do
+    for i = 1, ATEMMixerMediaPoolStillsCount(1) do
+        if media_player == 1 then
+            media_player = 2;
+        else
+            media_player = 1;
+        end;
+        ATEMMixerMPSetMediaIndex( 1,media_player,i);
+        if media_player == 1 then
+            ATEMMixerMESetPreviewInput(  1, 1, 11);
+        else
+            ATEMMixerMESetPreviewInput(  1, 1, 13);
+        end;
+        ATEMMixerMEAutoTransition(1,1);
+        VSLog("MediaPlayer: " .. media_player);
+        Sleep(10000);
+    end;
+    end;
+
+end;
+
+InfoGraphic()    
